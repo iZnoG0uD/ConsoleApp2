4e992e8ee8ca750fedb1c47a6ac0fd854c4e0900
iZnoG0uD <zelesnac@gmail.com>
2023-09-16 23:39:59 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+
+            MyName = "Jane";
+            Console.WriteLine(MyName);
+        }
+    }
+}
