cdc7b73cf432c341d8dae572a22ffe3b0023d166
iZnoG0uD <zelesnac@gmail.com>
2023-09-18 16:20:20 +0400

    Добавлены новые переменные

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,11 +5,17 @@
         static void Main(string[] args)
         {
             const string MyName = "Jane";
+            byte age = 27;
+            ushort shoeSize = 42;
+            bool isPet = true;
             Console.WriteLine(MyName);
 
             Console.WriteLine("\tПривет,\n Мир!");
-            Console.WriteLine("\tМне 27 лет");
-            Console.WriteLine("\tMy name is\n Jane");
+            Console.WriteLine("\tМне {0} лет", age);
+            Console.WriteLine("\tMy name is\n {0}", MyName);
+            Console.WriteLine("\tDo I have a pet?\n\t {0}", isPet);
+            Console.WriteLine("\tMy shoe size is {0}", shoeSize);
+
 
             Console.ReadKey();
         }
